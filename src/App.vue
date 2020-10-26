b187cd17b8c5a1535f7e589c900ffbf2fa29e490
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-10-27 00:36:43 +0300

    ()

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  <div id="app" class="flex bg-gray-900 text-white min-h-screen">
    <div class="nav w-24 fixed bg-blue-500 min-h-screen flex justify-center items-center">
      <ul>
-       <li><a href="#"></a></li>
+       <li><a href="#">MOHA</a></li>
      </ul>
    </div>
    <div class="w-full ml-24">
